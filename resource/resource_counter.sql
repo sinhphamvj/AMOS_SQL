deb2a2055d375e9116feea07907767ad4bb4b83c
sinhphamvj <sinhpham@vietjetair.com>
2025-08-22 13:30:33 +0700

    new

--- resource/resource_counter.sql
+++ resource/resource_counter.sql
@@ -1,1 +1,27 @@
---đếm số lượng resource của AMO
+
+SELECT 
+    -- Tổng số user dept = AMO VJC
+    MAX(CASE WHEN "DEPT" = 'AMO' THEN "RESOURCE_COUNT" ELSE 0 END) AS "AMO_TOTAL",
+    
+    -- Tổng số user MOC
+    MAX(CASE WHEN "DEPT" = 'MOC' THEN "RESOURCE_COUNT" ELSE 0 END) AS "MOC_TOTAL",
+    
+    -- Total resource AMO
+    (MAX(CASE WHEN "DEPT" = 'AMO' THEN "RESOURCE_COUNT" ELSE 0 END)+
+    MAX(CASE WHEN "DEPT" = 'MOC' THEN "RESOURCE_COUNT" ELSE 0 END)) AS "TOTAL_RESOURCE"
+
+FROM (
+    -- Query
+    SELECT COUNT(user_sign) AS "RESOURCE_COUNT", 'AMO' AS "DEPT"
+    FROM sign
+    WHERE 
+    department = 'VJC AMO'
+    AND status = 0
+
+    UNION ALL
+    -- Query
+    SELECT COUNT(*) AS "RESOURCE_COUNT", 'MOC' AS "DEPT"
+    FROM sign
+    WHERE 
+    status = 0
+    ) AS resource_count
